4943f4edc3ac16b2f70bf50a5619b9cbddf73851
01Clarian <clarionnorth@gmail>
2019-11-05 13:28:21 -0500

    Updates

---
+++ precache-manifest.2efb85e8d53e5dbd1b3715f5857a7ea9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8db089c890821402f211cac35325ba0b",
+    "url": "/ReduxMovieAppTutorial/index.html"
+  },
+  {
+    "revision": "fba116bac1394f0686e1",
+    "url": "/ReduxMovieAppTutorial/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "44c6dc8bbf34ea384a55",
+    "url": "/ReduxMovieAppTutorial/static/js/2.c3971cfc.chunk.js"
+  },
+  {
+    "revision": "fba116bac1394f0686e1",
+    "url": "/ReduxMovieAppTutorial/static/js/main.e89a2663.chunk.js"
+  },
+  {
+    "revision": "dc02bdb965d03aa8f080",
+    "url": "/ReduxMovieAppTutorial/static/js/runtime-main.b7ddc10b.js"
+  }
+]);
